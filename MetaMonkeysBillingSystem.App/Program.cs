3e724fe8f642005892b7037905bcc7add884be94
therapist3003 <tharunravichandran1@gmail.com>
2024-05-14 13:17:07 +0530

    Tried using WasmHelper to connect sqlite.

--- MetaMonkeysBillingSystem.App/Program.cs
+++ MetaMonkeysBillingSystem.App/Program.cs
@@ -1,6 +1,9 @@
 using MetaMonkeysBillingSystem.App;
+using MetaMonkeysBillingSystem.App.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SqliteWasmHelper;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddSqliteWasmDbContextFactory<MMStoreDBContext>(options => options.UseSqlite("Data Source=MM_Store_DB.db"));
 await builder.Build().RunAsync();
