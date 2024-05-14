7f1ede439e6e912b2aed12aaf1da8d82edbfa3cf
therapist3003 <tharunravichandran1@gmail.com>
2024-05-14 13:04:57 +0530

    Created sqlite database file.

---
+++ MetaMonkeysBillingSystem.App/Data/MMStoreDBContext.cs
@@ -0,0 +1,38 @@
+﻿//To represent a session with the database
+
+using MetaMonkeysBillingSystem.App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetaMonkeysBillingSystem.App.Data
+{
+    public class MMStoreDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration; // Created as part of obtaining the sqlite connection string
+
+        public MMStoreDBContext(DbContextOptions<MMStoreDBContext> options) : base(options)
+        {
+
+        }
+
+        public MMStoreDBContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // One DbSet for each table in the database
+        public DbSet<Customer> Customers { get; set; } = null!;
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+
+        public DbSet<Invoice> Invoices { get; set; } = null!;
+
+        public DbSet<InvoiceItem> InvoicesItems { get; set; } = null!;
+
+        public DbSet<Item> Items { get; set; } = null !;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("MM_Store_DB"));
+        }
+
+    }
+}
