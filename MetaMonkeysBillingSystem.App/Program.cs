4204e9112ed133ee10c92f31313c47224d177780
therapist3003 <tharunravichandran1@gmail.com>
2024-05-14 14:13:50 +0530

    Removed sqlite DB related files.

--- MetaMonkeysBillingSystem.App/Program.cs
+++ MetaMonkeysBillingSystem.App/Program.cs
@@ -1,9 +1,7 @@
 using MetaMonkeysBillingSystem.App;
-using MetaMonkeysBillingSystem.App.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.EntityFrameworkCore;
-using SqliteWasmHelper;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSqliteWasmDbContextFactory<MMStoreDBContext>(options => options.UseSqlite("Data Source=MM_Store_DB.db"));
 await builder.Build().RunAsync();
