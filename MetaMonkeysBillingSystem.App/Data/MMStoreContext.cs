3bbaf7e401a8be9d655f6785912a479e8df1dfb1
therapist3003 <tharunravichandran1@gmail.com>
2024-05-14 14:40:52 +0530

    Trying to integrate MySQL Database.

---
+++ MetaMonkeysBillingSystem.App/Data/MMStoreContext.cs
@@ -0,0 +1,20 @@
+﻿// Representing a session with the database
+
+using MetaMonkeysBillingSystem.App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetaMonkeysBillingSystem.App.Data
+{
+    public class MMStoreContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Invoice> Invoices { get; set; } = null!;
+        public DbSet<InvoiceItem> InvoiceItems { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
